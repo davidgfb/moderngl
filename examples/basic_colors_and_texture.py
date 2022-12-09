ad0821907cdd279b8eb2e2c46bc054ce2e3818df
davidgfb <davidgracia99@gmail.com>
2022-12-09 18:47:53 +0100

    Update basic_colors_and_texture.py

--- examples/basic_colors_and_texture.py
+++ examples/basic_colors_and_texture.py
@@ -1,127 +1,98 @@
-'''
-    Renders a floating, oscillating, 3d islans with lights
-'''
-import numpy as np
+from numpy import sin, ones
 from pyrr import Matrix44
-
-import moderngl
 from moderngl_window import WindowConfig
-
+from moderngl import DEPTH_TEST, CULL_FACE
 
 class ColorsAndTexture(WindowConfig):
-    resource_dir = 'data'
-    gl_version = (3, 3)
-    title = "Colors and Textures"
+    #Renders a floating, oscillating, 3d island with lights
+    resource_dir, gl_version, title = 'data', (3, 3),\
+                                      "Colors and Textures"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.prog = self.ctx.program(
-            vertex_shader='''
-                #version 330
+        self.prog = self.ctx.program(vertex_shader =\
+        '''#version 330
+        uniform mat4 Mvp = mat4(0);
 
-                uniform mat4 Mvp;
+        in vec3 in_position, in_normal;
+        in vec2 in_texcoord_0;
 
-                in vec3 in_position;
-                in vec3 in_normal;
-                in vec2 in_texcoord_0;
+        out vec3 v_vert, v_norm;
+        out vec2 v_text;
 
-                out vec3 v_vert;
-                out vec3 v_norm;
-                out vec2 v_text;
+        void main() {
+            gl_Position = Mvp * vec4(in_position, 1);
+            v_vert = in_position;
+            v_norm = in_normal;
+            v_text = in_texcoord_0;
+        }''', fragment_shader =\
+        '''#version 330
+        uniform vec3 Light = vec3(0), Color = vec3(0);
+        uniform bool UseTexture = false; //en minusculas
+        uniform sampler2D Texture;
 
-                void main() {
-                    gl_Position = Mvp * vec4(in_position, 1.0);
-                    v_vert = in_position;
-                    v_norm = in_normal;
-                    v_text = in_texcoord_0;
-                }
-            ''',
-            fragment_shader='''
-                #version 330
+        in vec3 v_vert, v_norm;
+        in vec2 v_text;
 
-                uniform vec3 Light;
-                uniform vec3 Color;
-                uniform bool UseTexture;
-                uniform sampler2D Texture;
+        out vec4 f_color;
 
-                in vec3 v_vert;
-                in vec3 v_norm;
-                in vec2 v_text;
+        void main() {
+            float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.8 + 0.2;
 
-                out vec4 f_color;
+            if (UseTexture) {
+                f_color = vec4(texture(Texture, v_text).rgb * lum, 1);
 
-                void main() {
-                    float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.8 + 0.2;
-                    if (UseTexture) {
-                        f_color = vec4(texture(Texture, v_text).rgb * lum, 1.0);
-                    } else {
-                        f_color = vec4(Color * lum, 1.0);
-                    }
-                }
-            ''',
-        )
-
-        self.mvp = self.prog['Mvp']
-        self.light = self.prog['Light']
-        self.color = self.prog['Color']
-        self.use_texture = self.prog['UseTexture']
-
+            } else {
+                f_color = vec4(Color * lum, 1);
+            }
+        }''')
         # Note: This is a fairly manual way to loading and rendering wavefront files.
         # There are easier ways when loading multiple objects in a single obj file.
-
         # Load obj files
-        self.scene_ground = self.load_scene('scene-1-ground.obj')
-        self.scene_grass = self.load_scene('scene-1-grass.obj')
-        self.scene_billboard = self.load_scene('scene-1-billboard.obj')
-        self.scene_holder = self.load_scene('scene-1-billboard-holder.obj')
-        self.scene_image = self.load_scene('scene-1-billboard-image.obj')
-
         # Extract the VAOs from the scene
-        self.vao_ground = self.scene_ground.root_nodes[0].mesh.vao.instance(self.prog)
-        self.vao_grass = self.scene_grass.root_nodes[0].mesh.vao.instance(self.prog)
-        self.vao_billboard = self.scene_billboard.root_nodes[0].mesh.vao.instance(self.prog)
-        self.vao_holder = self.scene_holder.root_nodes[0].mesh.vao.instance(self.prog)
-        self.vao_image = self.scene_image.root_nodes[0].mesh.vao.instance(self.prog)
-
+        self.vao_ground, self.vao_grass, self.vao_billboard,\
+                         self.vao_holder, self.vao_image =\
+        (*(obj.root_nodes[0].mesh.vao.instance(self.prog)\
+         for obj in (*(self.load_scene(obj + '.obj') for obj in\
+        ('scene-1-ground', 'scene-1-grass', 'scene-1-billboard',\
+                           'scene-1-billboard-holder',\
+                           'scene-1-billboard-image')),)),)
+                         
         # texture on billboard
         self.texture = self.load_texture_2d('infographic-1.jpg')
 
-    def render(self, time: float, frame_time: float):
-        self.ctx.clear(1.0, 1.0, 1.0)
-        self.ctx.enable(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
+    def render(self, time, frame_time):
+        self.ctx.clear(*ones(3))
+        self.ctx.enable(DEPTH_TEST | CULL_FACE)
 
-        proj = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 1000.0)
-        lookat = Matrix44.look_at(
-            (47.697, -8.147, 24.498),
-            (0.0, 0.0, 8.0),
-            (0.0, 0.0, 1.0),
-        )
+        proj, lookat, rotate, self.prog['UseTexture'],\
+              self.prog['Light'], self.prog['Color'] =\
+              Matrix44.perspective_projection(45,\
+                                  self.aspect_ratio, 1/10, 1000),\
+                       Matrix44.look_at((47.697, -8.147, 24.498),
+                                           (0, 0, 8), (0, 0, 1)),\
+              Matrix44.from_z_rotation(sin(time) / 2 + 1 / 5),\
+              False, (67.69, -8.14, 52.49), (0.67, 0.49, 0.29)
 
-        rotate = Matrix44.from_z_rotation(np.sin(time) * 0.5 + 0.2)
-
-        self.use_texture.value = False
-
-        self.light.value = (67.69, -8.14, 52.49)
-        self.mvp.write((proj * lookat * rotate).astype('f4'))
-
-        self.color.value = (0.67, 0.49, 0.29)
+        self.prog['Mvp'].write((proj * lookat * rotate).astype('f4'))
         self.vao_ground.render()
 
-        self.color.value = (0.46, 0.67, 0.29)
+        self.prog['Color'] = 0.46, 0.67, 0.29
+
         self.vao_grass.render()
 
-        self.color.value = (1.0, 1.0, 1.0)
+        self.prog['Color'] = ones(3)
+
         self.vao_billboard.render()
 
-        self.color.value = (0.2, 0.2, 0.2)
+        self.prog['Color'] = ones(3) / 5
+
         self.vao_holder.render()
 
-        self.use_texture.value = True
-        self.texture.use()
+        self.prog['UseTexture'] = True
 
+        self.texture.use()
         self.vao_image.render()
 
-
-if __name__ == '__main__':
-    ColorsAndTexture.run()
+ColorsAndTexture.run()
